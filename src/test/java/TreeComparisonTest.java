838ebb81f288f224c0b486232a294a23099baf7e
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2019-06-18 15:34:26 -0700

    More random tests

--- src/test/java/TreeComparisonTest.java
+++ src/test/java/TreeComparisonTest.java
@@ -10,30 +10,27 @@ import rrcf.memory.ShingledForest;
 public class TreeComparisonTest {
     @Test
     public void ComparisonTestForest() {
-        for (long i = 2; i < 1000; i=i*i) {
-            for (int numTrees = 1; numTrees < 6; numTrees += 4) {
-                for (int treeSize = 1; treeSize < 100; treeSize += 18) {
-                    for (int shingleSize = 2; shingleSize < 50; shingleSize += 16) {
-                        System.out.printf("Testing with %d %d %d %d\n", i, numTrees, treeSize, shingleSize);
-                        Random r = new Random(i);
-                        ShingledForest test1 = new ShingledForest(r, shingleSize, numTrees, treeSize);
-                        Random r2 = new Random(i);
-                        SimpleShingledForest test2 = new SimpleShingledForest(r2, shingleSize, numTrees, treeSize);
-                        Random rTest = new Random(1);
-                        for (int e = 0; e < 300; e++) {
-                            double val = rTest.nextDouble() * 1000;
-                            //System.out.printf("Inserting %f\n", val);
-                            double v2 = test2.addPoint(val);
-                            double v1 = test1.addPoint(val);
-                            //System.out.println("Expected:");
-                            //System.out.println(test2.trees[0].toString());
-                            //System.out.println("Actual:");
-                            //System.out.println(test1.trees[0].toString());
-                            assertEquals(v2, v1, 0.000001);
-                            assertEquals(test2.trees[0].toString(), test1.trees[0].toString());
-                        }
-                    }
-                }
+        Random rTest = new Random(1);
+        for (long i = 0; i < 100; i++) {
+            long randomSeed = rTest.nextLong();
+            int numTrees = rTest.nextInt(15) + 1;
+            int treeSize = rTest.nextInt(200) + 20;
+            int shingleSize = rTest.nextInt(50) + 2;
+            int numInserts = rTest.nextInt(treeSize * 3) + treeSize;
+            System.out.printf("Testing with (seed %d, trees %d, treeSize %d, shingleSize %d)\n", i, numTrees, treeSize, shingleSize);
+            ShingledForest testUnknown = new ShingledForest(new Random(randomSeed), shingleSize, numTrees, treeSize);
+            SimpleShingledForest testVerify = new SimpleShingledForest(new Random(randomSeed), shingleSize, numTrees, treeSize);
+            for (int e = 0; e < numInserts; e++) {
+                double val = rTest.nextDouble() * 1000;
+                //System.out.printf("Inserting %f\n", val);
+                double v2 = testVerify.addPoint(val);
+                double v1 = testUnknown.addPoint(val);
+                //System.out.println("Expected:");
+                //System.out.println(test2.trees[0].toString());
+                //System.out.println("Actual:");
+                //System.out.println(test1.trees[0].toString());
+                assertEquals(v2, v1, 0.000001);
+                assertEquals(testVerify.trees[0].toString(), testUnknown.trees[0].toString());
             }
         }
     }
